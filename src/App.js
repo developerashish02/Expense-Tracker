b2cfe5deabb58275e3bf3ef21ff7638402a42cd5
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2022-08-26 12:25:18 +0530

    Add date humen redable format

--- src/App.js
+++ src/App.js
@@ -2,9 +2,54 @@ import ExpenceItem from "./components/ExpenceItems";
 import "./App.css";
 
 function App() {
+	const expense = [
+		{
+			id: "e1",
+			title: "Car Inseranse",
+			amount: "10,000",
+			date: new Date(2020 , 7 ,14),
+		},
+		{
+			id: "e1",
+			title: "Bike Inseranse",
+			amount: "5,000",
+			date: new Date(2021 , 7 ,14),
+		},
+		{
+			id: "e1",
+			title: "Helth Inseranse",
+			amount: "12,000",
+			date: new Date(2022 , 7 ,14),
+		},
+		{
+			id: "e1",
+			title: "Family Inseranse",
+			amount: "20,000",
+			date: new Date(2022, 7 ,14),
+		},
+	];
 	return (
 		<div className="App">
-			<ExpenceItem />
+			<ExpenceItem
+				title={expense[0].title}
+				amount={expense[0].amount}
+				date={expense[0].date}
+			/>
+			<ExpenceItem
+				title={expense[1].title}
+				amount={expense[1].amount}
+				date={expense[1].date}
+			/>
+			<ExpenceItem
+				title={expense[2].title}
+				amount={expense[2].amount}
+				date={expense[2].date}
+			/>
+			<ExpenceItem
+				title={expense[3].title}
+				amount={expense[3].amount}
+				date={expense[3].date}
+			/>
 		</div>
 	);
 }
--- src/components/ExpenceItems.js
+++ src/components/ExpenceItems.js
@@ -1,12 +1,20 @@
 import "../styles/ExpenseItems.css";
 
-const ExpenceItem = () => {
+const ExpenceItem = (props) => {
+	// getting the dates humen readeble format
+	const month = props.date.toLocaleString("en-US", { month: "long" });
+	const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+	const year = props.date.getFullYear();
 	return (
 		<div className="expense-item">
-			<div>March 28th 2022</div>
+			<div>
+				<div>{month}</div>
+				<div>{day}</div>
+				<div>{year}</div>
+			</div>
 			<div className="expense-item__description">
-				<h2>Car Insurance</h2>
-				<div className="expense-item__price">10,000</div>
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">Rs {props.amount}</div>
 			</div>
 		</div>
 	);
