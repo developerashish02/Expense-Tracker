eba2f24c4ad135690cde4f2c227049e6cb5a0fac
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2022-08-26 12:43:26 +0530

    Add Date component seprate and style

--- src/components/ExpenceItems.js
+++ src/components/ExpenceItems.js
@@ -1,17 +1,11 @@
 import "../styles/ExpenseItems.css";
+import ExpenceDate from "./ExpenseDate";
 
 const ExpenceItem = (props) => {
-	// getting the dates humen readeble format
-	const month = props.date.toLocaleString("en-US", { month: "long" });
-	const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-	const year = props.date.getFullYear();
 	return (
 		<div className="expense-item">
-			<div>
-				<div>{month}</div>
-				<div>{day}</div>
-				<div>{year}</div>
-			</div>
+			{/* Date component */}
+			<ExpenceDate date={props.date} />
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">Rs {props.amount}</div>
